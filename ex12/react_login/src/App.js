aafb31b93336a81f172913c8e6281f78e410e020
hssSean <a0966211623@gmail.com>
2026-05-13 13:56:15 +0800

    uoload

---
+++ ex12/react_login/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import CGU_Login from './cgu_login'; 
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        { CGU_Login() }
+      </div>
+    </div>
+  );
+}
+
+export default App;
